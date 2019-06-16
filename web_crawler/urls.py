00484a14ee3d1dcef7d4996ef875d50957ed964e
Mayank Dubey <mayankdubey291@gmail.com>
2019-06-16 23:28:47 +0530

    Add url for parser view

--- web_crawler/urls.py
+++ web_crawler/urls.py
@@ -14,14 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework_swagger.views import get_swagger_view
 from . import views
+from crawler import views as crawler_views
 
 schema_view = get_swagger_view(title='Webcrawler API')
 
 urlpatterns = [
     path('', schema_view),
     path('hello-world', views.hello_world),
+    path('parse-url', crawler_views.parse_url),
     path('admin/', admin.site.urls),
 ]
